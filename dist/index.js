cf1b641cd1ba96a1b63322415b376cf51ecdbe61
Dipesh Acharya <xtranophilist@gmail.com>
2017-10-05 13:27:33 +0545

    scss compilation support

--- dist/index.js
+++ dist/index.js
@@ -65,9 +65,19 @@
 /************************************************************************/
 /******/ ([
 /* 0 */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__index_scss__ = __webpack_require__(1);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__index_scss___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_0__index_scss__);
+
+
+/***/ }),
+/* 1 */
 /***/ (function(module, exports) {
 
-
+// removed by extract-text-webpack-plugin
 
 /***/ })
 /******/ ]);
