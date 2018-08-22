689aff4a2a9891d736cbbc615edd2938f8e71fef
Dipesh Acharya <xtranophilist@gmail.com>
2018-08-22 19:37:47 +0545

    Use webpack-html-plugin to generate html

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const webpack = require('webpack');
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
 
 const path = require('path');
 const resolve = path.resolve.bind(path, __dirname);
@@ -30,11 +32,15 @@ module.exports = {
           use: "css-loader!sass-loader"
         })
       },
-      {test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "url-loader?limit=10000&mimetype=application/font-woff"},
-      {test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "file-loader"}
+      { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "url-loader?limit=10000&mimetype=application/font-woff" },
+      { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "file-loader" }
     ]
   },
   plugins: [
-    new ExtractTextPlugin({filename: '[name].css', allChunks: true}),
+    new ExtractTextPlugin({ filename: '[name].css', allChunks: true }),
+    new HtmlWebpackPlugin({  // Also generate a test.html
+      filename: '../index.html',
+      template: 'index_src.html'
+    })
   ]
 };
