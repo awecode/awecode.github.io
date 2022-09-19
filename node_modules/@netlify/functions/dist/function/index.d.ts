c0ac42399d0b5e2f567257e1098ebdc5be61e1fb
sboandiakl05 <sonikbadal@gmail.com>
2022-09-19 13:18:09 +0545

    Remove Node Modules folder

--- node_modules/@netlify/functions/dist/function/index.d.ts
+++
@@ -1,6 +0,0 @@
-export { Context as HandlerContext } from './context';
-export { Event as HandlerEvent } from './event';
-export { Handler, HandlerCallback } from './handler';
-export { Response as HandlerResponse } from './response';
-export { getSecrets, getSecretsForBuild, withSecrets, getNetlifyGraphToken, getNetlifyGraphTokenForBuild, GraphTokenResponse, HasHeaders, } from '../lib/graph';
-export { NetlifySecrets } from '../lib/secrets_helper';
